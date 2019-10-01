40c22fdc023fe3e8888ffd4606890535c2f2c332
diegobatt <diego.battocchio@gmail.com>
2019-10-01 00:13:14 -0300

    quicksort almost finished

--- algorithms-design-and-analysis-1/hw2-quicksort.py
+++ algorithms-design-and-analysis-1/hw2-quicksort.py
@@ -1,36 +1,74 @@
 """WIP"""
-def choose_pivot(array, type='first'):
+def swap_pivot(array, from_ix, to_ix, type='first'):
     if type == 'first':
-        return 0
+        pass
     elif type == 'last':
-        return -1
+        array[from_ix], array[to_ix] = array[to_ix], array[from_ix]
     else:
         raise NotImplementedError
+    return array[from_ix]
 
-def quicksort(array, pivot_type='first'):
+def quicksort(array, from_ix=None, to_ix=None, pivot_type='first'):
 
-    n = len(array)
+    comps = 0
+    from_ix = from_ix if from_ix is not None else 0
+    to_ix = to_ix if to_ix is not None else len(array) - 1
 
-    if n == 1:
-        return array
-    elif n == 2
-        if array[0] > array[1]:
-            return array[::-1]
-        else:
-            return array
+    size = to_ix - from_ix + 1
+
+    if size < 2:
+        return comps
+    elif size == 2:
+        if array[from_ix] > array[to_ix]:
+            array[from_ix], array[to_ix] = array[to_ix], array[from_ix]
+        comps += size - 1
+
+    else:
+        comps += size - 1
+        pivot = swap_pivot(array, from_ix, to_ix, type=pivot_type)
+        pivot_ix = from_ix
+
+        j = from_ix
+        for i in range(from_ix, to_ix + 1):
+            if i == pivot_ix:
+                j += 1
+            elif array[i] < pivot:
+                array[i], array[j] = array[j], array[i]
+                j += 1
+
+        array[j - 1], array[pivot_ix] = array[pivot_ix], array[j - 1]
+
+        comps_1 = quicksort(array, from_ix=from_ix, to_ix=j-2, pivot_type=pivot_type)
+        comps_2 = quicksort(array, from_ix=j, to_ix=to_ix, pivot_type=pivot_type)
+        comps = comps + comps_1 + comps_2
+
+    return comps
+
+
+if __name__ == "__main__":
+    import os
+    import random
+    import sys
+
+    import numpy as np
+
+    N = 100
+    try:
+        filepath = sys.argv[1]
+        integers = np.genfromtxt(filepath)
+        N = len(integers)
+    except:
+        integers = np.arange(1, N)
+        np.random.shuffle(integers)
     
-    pivot_ix = choose_pivot(array, type=pivot_type)
-    pivot = array[pivot_ix]
+    print(integers)
+    comps = quicksort(integers, pivot_type='last')
+    print("COMPARISONS:", comps)
+    import ipdb; ipdb.set_trace()
+    print(all(integers == np.arange(1, N)))
 
-    i, j = 1, 1
-    while j < n:
-        if array[j] < pivot:
-            array[i], array[j] = array[j], array[i]
-            i += 1
-            j += 1
-        else:
-            j += 1
-    array[pivot_ix]
+
+
     
 
 
