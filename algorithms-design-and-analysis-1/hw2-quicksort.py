6a2cd24b3169cb46d3635b41d5023c5623fd7b09
diegobatt <diego.battocchio@gmail.com>
2019-10-01 23:05:43 -0300

    quicksort finished

--- algorithms-design-and-analysis-1/hw2-quicksort.py
+++ algorithms-design-and-analysis-1/hw2-quicksort.py
@@ -1,9 +1,32 @@
-"""WIP"""
-def swap_pivot(array, from_ix, to_ix, type='first'):
-    if type == 'first':
+def median_of_3(a, b, c):
+    if a > b:
+        if b > c:
+            return 1, b
+        elif c > a:
+            return 0, a
+        else:
+            return 2, c
+    else:
+        if b < c:
+            return 1, b
+        elif c < a:
+            return 0, a
+        else:
+            return 2, c
+
+
+def swap_pivot(array, from_ix, to_ix, pivot_type='first'):
+    if pivot_type == 'first':
         pass
-    elif type == 'last':
+    elif pivot_type == 'last':
         array[from_ix], array[to_ix] = array[to_ix], array[from_ix]
+    elif pivot_type == 'median':
+        size = to_ix - from_ix + 1
+        half_ix = ((size // 2 - 1) if size % 2 == 0 else (size // 2)) + from_ix
+        first, half, last = array[from_ix], array[half_ix], array[to_ix]
+        ixs = [from_ix, half_ix, to_ix]
+        med_ix = ixs[median_of_3(first, half, last)[0]]
+        array[from_ix], array[med_ix] = array[med_ix], array[from_ix]
     else:
         raise NotImplementedError
     return array[from_ix]
@@ -22,10 +45,9 @@ def quicksort(array, from_ix=None, to_ix=None, pivot_type='first'):
         if array[from_ix] > array[to_ix]:
             array[from_ix], array[to_ix] = array[to_ix], array[from_ix]
         comps += size - 1
-
     else:
         comps += size - 1
-        pivot = swap_pivot(array, from_ix, to_ix, type=pivot_type)
+        pivot = swap_pivot(array, from_ix, to_ix, pivot_type=pivot_type)
         pivot_ix = from_ix
 
         j = from_ix
@@ -52,20 +74,19 @@ if __name__ == "__main__":
 
     import numpy as np
 
-    N = 100
+    N = 11
     try:
         filepath = sys.argv[1]
-        integers = np.genfromtxt(filepath)
-        N = len(integers)
+        integers = np.genfromtxt(filepath, dtype=int)
+        N = len(integers) + 1
     except:
         integers = np.arange(1, N)
         np.random.shuffle(integers)
-    
+
     print(integers)
-    comps = quicksort(integers, pivot_type='last')
+    comps = quicksort(integers, pivot_type='median')
     print("COMPARISONS:", comps)
-    import ipdb; ipdb.set_trace()
-    print(all(integers == np.arange(1, N)))
+    print(all(np.array(integers) == np.arange(1, N)))
 
 
 
