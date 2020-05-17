dcfc7d4df48c4f656d0f0a4a69b74a49905665fe
diegobatt <diego.battocchio@gmail.com>
2020-05-17 16:21:50 -0300

    sphere fit problem

--- convex-optimization/week8-geometric-problem/sphere-fit.py
+++ convex-optimization/week8-geometric-problem/sphere-fit.py
@@ -9,16 +9,18 @@ plt.figure()
 plt.scatter(x[:, 0], x[:, 1])
 plt.show()
 
-
+# Problem is an LP expressed like this
 x_norms = cp.norm(x, axis=1) ** 2
 c = cp.Variable(2)
 rho = cp.Variable()
-objective = cp.Minimize(cp.norm(x_norms - x @ c + rho))
+objective = cp.Minimize(cp.norm(x_norms - x @ c - rho))
 cp.Problem(objective).solve()
+# Retrieve the center and radius with simple transformations
 center = 1/2 * c.value
 radius = np.sqrt(rho.value + center @ center)
 x_ = radius * (x - center) / np.linalg.norm(x - center, axis=1)[:, None] + center
 
+# Plot results
 plt.figure()
 plt.scatter(x[:, 0], x[:, 1])
 plt.scatter(x_[:, 0], x_[:, 1])
